852816411fa07e87b08fe0be111bcdee3324de74
Susil Chand <susilkyan1@gmail.com>
2024-03-12 20:44:15 +0630

    Add Initial Files

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+// For 
+function BlockBar() {
+    var alertBar = document.querySelector('.alertbar');
+    if (alertBar) {
+        alertBar.style.display = 'none';
+    }
+}
+// for Profile Picture Chooser
+
+const imgDiv = document.querySelector('profile-pic-div');
+const img = document.querySelector('#previewPhoto');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+file.addEventListener('change', function () {
+    const choosedFile = this.files[0];
+    if (choosedFile) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function
+            () {
+            img.setAttribute('src', reader.result);
+        });
+        reader.readAsDataURL(choosedFile);
+    }
+});
+
+function toggleThumbsUp() {
+  var thumbsUpIcon = document.getElementById('thumbsUpIcon');
+  var thumbsUpCount = document.getElementById('thumbsUpCount');
+  thumbsUpIcon.classList.toggle('active'); // Toggle the 'active' class
+  var count = parseInt(thumbsUpCount.innerText);
+
+  if (thumbsUpIcon.classList.contains('active')) {
+      count++;
+  } else {
+      count--;
+  }
+
+  thumbsUpCount.innerText = count;
+}
+
+function toggleBookmark() {
+  var bookmarkIcon = document.getElementById('bookmarkIcon');
+  bookmarkIcon.classList.toggle('active-bookmark'); // Toggle the 'active-bookmark' class
+}
+//   for Profile Picture 
+
+document.addEventListener('click', function(event) {
+    var dropdownContent = document.getElementById("dropdownContent");
+    if (!dropdownContent.contains(event.target)) {
+      dropdownContent.classList.remove("show");
+    }
+  });
+  
+  function toggleDropdown(event) {
+    var dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.classList.toggle("show");
+    event.stopPropagation(); // Stop the click event from propagating further
+  }
+  // Update Profile
+  document.addEventListener('click', function(event) {
+    var dropdownContent = document.getElementById("dropContent");
+    if (!dropdownContent.contains(event.target)) {
+      dropdownContent.classList.remove("show");
+    }
+  });
+  
+  function togglDropdown(event) {
+    var dropdownContent = document.getElementById("dropContent");
+    dropdownContent.classList.toggle("show");
+    event.stopPropagation(); // Stop the click event from propagating further
+  }
+
+
+
+
+
+
